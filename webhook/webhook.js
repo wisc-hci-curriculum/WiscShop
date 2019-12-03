b3ecba1cb67907e40b292b3e8e9a31d5eb5c6a4b
JuCJeff <zma67@wisc.edu>
2019-12-02 18:46:05 -0600

    Edited to make the token work globally

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -4,36 +4,48 @@ const app = express()
 const fetch = require('node-fetch')
 const base64 = require('base-64')
 
+let username = "";
+let password = "";
+let token = "";
+
+async function getToken () {
+  let request = {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json',
+              'Authorization': 'Basic '+ base64.encode(username + ':' + password)},
+    redirect: 'follow'
+  }
+
+  const serverReturn = await fetch('https://mysqlcs639.cs.wisc.edu/login',request)
+  const serverResponse = await serverReturn.json()
+  token = serverResponse.token
+
+  return token;
+}
+
 app.get('/', (req, res) => res.send('online'))
 app.post('/', express.json(), (req, res) => {
   const agent = new WebhookClient({ request: req, response: res })
-
-  let username = "";
-  let password = "";
-  let token = "";
 
   function welcome () {
     agent.add('Webhook works!')
   }
 
-  async function login (username, password) {
-    let request = {
-      method: 'GET',
-      headers: {'Content-Type': 'application/json',
-                'Authorization': 'Basic '+ base64.encode(username + ':' + password)},
-      redirect: 'follow'
-    }
-
-    const serverReturn = await fetch('https://mysqlcs639.cs.wisc.edu/login',request)
-    const serverResponse = await serverReturn.json()
-    const token = serverResponse.token
-
-    return token;
+  async function login () {
+    // You need to set this from `username` entity that you declare in DialogFlow
+    username = null
+    // You need to set this from password entity that you declare in DialogFlow
+    password = null
+    await getToken()
+    
+    agent.add(token)
   }
 
 
   let intentMap = new Map()
   intentMap.set('Default Welcome Intent', welcome)
+  // You will need to declare this `Login` content in DialogFlow to make this work
+  intentMap.set('Login', login) 
   agent.handleRequest(intentMap)
 })
 
